ac0d552339ded4ecf6a2a33d9830602c6d7c7357
Chocobozzz <florian.bigard@gmail.com>
2016-08-12 19:59:33 +0200

    Server: try to see why the travis build fails

--- server/tests/api/index.js
+++ server/tests/api/index.js
@@ -1,9 +1,4 @@
 'use strict'
 
 // Order of the tests we want to execute
-require('./checkParams')
-require('./friendsBasic')
-require('./users')
 require('./singlePod')
-require('./multiplePods')
-require('./friendsAdvanced')
